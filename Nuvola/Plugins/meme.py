125f41055c9d19df681cfafe6d902f4517e2ec0c
lajla <117605737+lajlapy@users.noreply.github.com>
2022-11-08 22:14:28 +0100

    Update meme.py

--- Nuvola/Plugins/meme.py
+++ Nuvola/Plugins/meme.py
@@ -10,9 +10,9 @@ import time
 @Nuvola.on_message(filters.me & filters.command("meme", PREFIX))
 async def meme(client: Nuvola, message):
     await message.edit_text("__Uploading...__ 🔄")
-    # doing request to reddit
+    # Doing request to reddit
     p = requests.get("https://meme-api.herokuapp.com/gimme/memes")
     rr = p.json()
     await message.delete()
-    #
+    # Send a random meme from the subreddit "Memes", through the request above
     await client.send_photo(message.chat.id, rr.get('url'), caption="<a href='https://reddit.com/r/Memes'>Reddit</a> 👩‍🦽")
