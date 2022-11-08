e92f25ed6068e9f0361d91ed09cf178b2cefcd80
Partizionare <lellodeveloper@gmail.com>
2022-11-08 19:10:51 +0100

    Fix filters and remove useless imports

--- Nuvola/Plugins/info.py
+++ Nuvola/Plugins/info.py
@@ -1,13 +1,10 @@
-# Import Class
+# Nuvola main class
 from ..nuvola import Nuvola
-# Import Nuvola instance
-from ..__main__ import nuvola
 from ..Utils.globals import PREFIX
-from pyrogram import filters, enums
+from pyrogram import filters
 from pyrogram.types import Message
-import asyncio
 
-# some infos about the bot
-@Nuvola.on_message(filters.command("info", PREFIX))
-async def info(client: Nuvola, message: Message):
-    await message.edit_text(f"**Nuvola** 0.1.0 ⛅️\n\nDeveloped **with ❤️ by:**\n\n• @Partizionare\n• @lajla\n\n**Language:** ➺ Python\n\n**Framework**: ➺ Pyrogram\n\n🔗 <a href='https://github.com/Partizionare/Nuvola'>GitHub</a>", disable_web_page_preview= True)
+
+@Nuvola.on_message(filters.me & filters.command("info", PREFIX))
+async def info(_, message: Message):
+    await message.edit_text(f"**Nuvola** 0.1.0 ⛅️\n\nDeveloped **with ❤️ by:**\n\n• @Partizionare\n• @lajla\n\n**Language:** ➺ Python\n\n**Framework**: ➺ Pyrogram\n\n🔗 <a href='https://github.com/Partizionare/Nuvola'>GitHub</a>", disable_web_page_preview=True)
