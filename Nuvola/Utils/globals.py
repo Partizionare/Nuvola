292b8550884c7ce803a89aadfaa18782cdea5d7c
Partizionare <lellodeveloper@gmail.com>
2022-11-11 18:42:30 +0100

    add global strings for args errors in plugins

--- Nuvola/Utils/globals.py
+++ Nuvola/Utils/globals.py
@@ -1,2 +1,6 @@
 # Commands prefix
 PREFIX = ["!", ".", "$"]
+# Invalid argument(s) message
+ARG_INVALID = "⚠️ Invalid argument(s). /help"
+# Argument(s) not provided message
+ARG_MISSING = "⚠️ Missing argument(s). /help"
