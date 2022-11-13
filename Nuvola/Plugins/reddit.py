de11aee96902fb421d14ff6fbc8af52670c41d26
Partizionare <lellodeveloper@gmail.com>
2022-11-13 16:12:17 +0100

    control flow rework, code optimization

--- Nuvola/Plugins/reddit.py
+++ Nuvola/Plugins/reddit.py
@@ -1,11 +1,15 @@
-from pyrogram import filters
+# Nuvola main class
 from ..nuvola import Nuvola
+# Nuvola istance
 from ..__main__ import nuvola
-from ..Utils.globals import PREFIX
+from ..Utils.globals import *
+from pyrogram import filters
+from pyrogram.types import Message
 import requests
-import time
-from pyrogram.errors import WebpageCurlFailed, MediaEmpty
+import asyncio
 
+
+# Add Reddit to commands list
 Nuvola.update_commands(nuvola, "REDDIT", {
     'name': 'reddit',
     'usage': ".reddit &lt-m&gt, &lt-w&gt, &lt-c (channel_name)&gt",
@@ -13,55 +17,54 @@ Nuvola.update_commands(nuvola, "REDDIT", {
     'category': 'Fun'
 })
 
+
 @Nuvola.on_message(filters.me & filters.command("reddit", PREFIX))
-async def reddit(client: Nuvola, message):
+async def reddit(client: Nuvola, message: Message):
+    # List of args, 'arg' : 'reddit_name'
+    args = {
+        '-m': 'memes',
+        '-w': 'wallpaper',
+    }
+    # Initialize reddit variable, it contains the reddit name in which the script will search for random posts
+    reddit = None
+    # If not, check whether a single argument is provided or not
     if (len(message.command) == 2):
-        if message.command[1] == "-m":
-            try:
-                await message.edit_text(f"__Uploading...__ 🔁")
-                meme_request = requests.get("https://meme-api.herokuapp.com/gimme/memes")
-                meme = meme_request.json()
-                await message.delete()
-                await client.send_photo(message.chat.id, meme.get('url'), caption=f"<a href='https://reddit.com/r/memes'>Reddit</a>")
-            except Exception as e:
-                await message.edit_text(f"{e}")
-        elif message.command[1] == "-w":
-            await message.edit_text("__Uploading...__ ")
-            get_wallpaper = requests.get("https://meme-api.herokuapp.com/gimme/wallpaper")
-            wallpaper = get_wallpaper.json()
+        # If yes, set variable 'reddit' value based on the arg provided
+        reddit = args.get(message.command[1])
+    # Check whether multiple arguments are provided or not
+    elif (len(message.command) == 3):
+        # If yes, check wheter the first argument is '-c' or not
+        if (message.command[2] == "-c"):
+            # If yes, set variable 'reddit' value based on the arg provided by the user via command
+            reddit = message.command[3]
+
+    # Check whether the reddit variable contains reddit name or not
+    if (reddit):
+        # If yes, try to aquire post info based on 'reddit' value
+        try:
+            # Edit message
+            await message.edit_text(f"Getting a random post from <a href='https://reddit.com/r/{reddit}'>{reddit}</a>", disable_web_page_preview=True)
+            # Fetch info using reddit api
+            reddit_request = requests.get(
+                f"https://meme-api.herokuapp.com/gimme/{reddit}")
+            # post_info contains all post informations
+            post_info = reddit_request.json()
+            # Send the post fetched by the script
+            await message.reply_photo(post_info.get('url'), caption=f"⛅️ Provided by Nuvola from <a href='{post_info['postLink']}'>Reddit</a>", quote=False)
+            # Delete message
             await message.delete()
-            await client.send_photo(message.chat.id, wallpaper.get('url'), caption = f"Here your **WallPaper** 🌇")
-        else:
-            invalid_syntax = await message.edit_text("⚠️ Syntax error.")
-            time.sleep(3)
-            await invalid_syntax.delete()
-    if (len(message.command) == 3):
-        if message.command[1] == "-c":
-            if message.command[2]:
-                try:
-                    await message.edit_text(f"__Uploading post from reddit.com/r/{message.command[2]}...__", disable_web_page_preview = True)
-                    reddit_channel = message.command[2]
-                    reddit_request = requests.get(f"https://meme-api.herokuapp.com/gimme/{reddit_channel}") 
-                    post = reddit_request.json()
-                    await message.delete()
-                    await client.send_photo(message.chat.id, post.get('url'), caption=f"<a href='https://reddit.com/r/{reddit_channel}'>Reddit</a>")
-                except WebpageCurlFailed:
-                    a = await client.send_message(message.chat.id, f"⚠️ Connection error, try again.")
-                    time.sleep(3)
-                    await a.delete()
-                except MediaEmpty:
-                    b = await client.send_message(message.chat.id, f"⚠️ Seems there is no post in this reddit...")
-                    time.sleep(3)
-                    await b.delete()
-                except AttributeError:
-                    c = await client.send_message(message.chat.id, f"⚠️ Seems this reddit doesn't exists...")
-                    time.sleep(3)
-                    await c.delete()
-            else:
-                channel_invalid = await message.edit_text("⚠️ Please provide a reddit channel name.")
-                time.sleep(3)
-                await channel_invalid.delete()
-        else:
-            invalid_syntax = await message.edit_text("⚠️ Syntax error.")
-            time.sleep(3)
-            await invalid_syntax.delete()
+        # Exception: the reddit doesn't exists
+        except (KeyError, AttributeError):
+            error_message = await message.edit_text("⚠️ This reddit doesn't exists.")
+        # Exception: general problems
+        except Exception:
+            error_message = await message.edit_text("⚠️ An error has occured while getting the post.")
+    # If not, there is a syntax error in the command
+    else:
+        error_message = await message.edit_text(ARG_SYNTAX)
+
+    # Check whether an error has occured or not
+    if (error_message):
+        # If yes, wait for x second(s) and delete the message
+        await asyncio.sleep(2)
+        await message.delete()
